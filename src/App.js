06d4b2ba6510e5b2c8c0fac9e75cb793f06942f6
oscarperezlugo <56516855+oscarperezlugo@users.noreply.github.com>
2022-02-21 17:32:58 -0400

    test

--- src/App.js
+++ src/App.js
@@ -1,23 +1,93 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
+
+
+
+
 
 function App() {
+  const [arr, setArr] = useState('');
+  const [arr2, setArr2] = useState('');
+  const [sizearr, setSizeArr] = useState('');
+  var arrtwo = Array.from(arr);
+  var arrthree = Array.from(arr2);
+  var matrixtwo = printMatrix(arrtwo, sizearr);
+  var matrixthree = printMatrix(arrthree, sizearr);
+  var num = printPrimes(arr);
+  var intersection = (arr2)
+  function testfunc(){
+   
+    
+    
+    
+  }
+  function printMatrix(list, elementsPerSubArray) {
+    var matrix = [], i, k;
+    for (i = 0, k = -1; i < list.length; i++) {
+        if (i % elementsPerSubArray === 0) {
+            k++;
+            matrix[k] = [];
+        }
+        matrix[k].push(list[i]);
+    }
+    return (
+      <div>
+      <ul>
+        {matrix.map((printMatrix) => (
+          <li>{printMatrix}</li>
+        ))}
+      </ul>
+      </div>
+    );
+  }
+
+  function printPrimes(num) {
+    for (let i = 2; i * i <= num; i++)
+        if (num % i === 0)
+          return false; 
+    return num > 1;
+  }
+
+  function findCircleCollisions(a, b) {
+    var setA = new Set(a);
+    var setB = new Set(b);
+    var intersection = new Set([...setA].filter(x => setB.has(x)));
+    return Array.from(intersection);
+  }
+
+
+
+  
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        
+        <form>
+          <label>
+            Values Array 1:
+            <input type="text" name="arrayvalues" onChange={e => setArr(e.target.value)}/><br></br>
+            Values Array 2:
+            <input type="text" name="matrixsize"  onChange={e => setArr2(e.target.value)}/>
+            <br></br>
+            Size:
+            <input type="text" name="matrixsize"  onChange={e => setSizeArr(e.target.value)}/>
+            </label><br></br>
+            
+   
+            <button onClick={testfunc} >Click me</button>
+      
+        </form>
+        
       </header>
+      <section>
+      {matrixtwo}
+      {matrixthree}
+      {num}
+      {intersection}
+      </section>
+   
     </div>
   );
 }
---
+++ src/Components.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+class printMatrix extends React.Component{
+    render(){
+        return(
+            <h1>Login</h1>
+        );
+    }
+}
+export default printMatrix;
